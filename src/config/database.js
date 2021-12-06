36125e39e649ceddfa8e17db1e793a0d7041e276
Eduardo Donato <eduardo.donato@gmail.com>
2021-12-06 15:06:03 -0300

    Variável de Ambiente para URL do Mongo
    - Atualizado pacote Mongoose
    - .env.defaults

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,12 @@
+require("dotenv").config()
+
 const mongoose = require("mongoose")
 
 mongoose.Promise = global.Promise
 
-module.exports = mongoose.connect("mongodb://localhost:27017/my-money", {
-    useMongoClient: true,
+const mongoConnectString = process.env.MONGO_CONNECT_STRING || "mongodb://localhost:27017/my-money"
+
+module.exports = mongoose.connect(mongoConnectString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
