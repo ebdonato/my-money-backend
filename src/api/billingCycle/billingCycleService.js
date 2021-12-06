4bf944911ed49a497dbb8081f2cfdac30665f6fd
Eduardo Donato <eduardo.donato@gmail.com>
2021-12-06 15:17:11 -0300

    Erro causado pela atualização do Model.aggregate do Mongo

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -20,26 +20,28 @@ BillingCycle.route("count", (req, res) => {
 
 BillingCycle.route("summary", (req, res) => {
     BillingCycle.aggregate(
-        {
-            $project: {
-                credit: {$sum: "$credits.value"},
-                debt: {$sum: "$debts.value"},
+        [
+            {
+                $project: {
+                    credit: {$sum: "$credits.value"},
+                    debt: {$sum: "$debts.value"},
+                },
             },
-        },
-        {
-            $group: {
-                _id: null,
-                credit: {$sum: "$credit"},
-                debt: {$sum: "$debt"},
+            {
+                $group: {
+                    _id: null,
+                    credit: {$sum: "$credit"},
+                    debt: {$sum: "$debt"},
+                },
             },
-        },
-        {
-            $project: {
-                _id: 0,
-                credit: 1,
-                debt: 1,
+            {
+                $project: {
+                    _id: 0,
+                    credit: 1,
+                    debt: 1,
+                },
             },
-        },
+        ],
         (error, result) => {
             if (error) {
                 res.status(500).json({errors: [error]})
